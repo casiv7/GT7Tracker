1aa3d61574acb85b62b5ecb14b779f1955b75f44
Ivan <ivanc10112@gmail.com>
2024-05-09 18:09:11 -0700

    Added .env folder

--- Server/index.js
+++ Server/index.js
@@ -1,16 +1,18 @@
 import express from "express";
+import env from "dotenv";
 import pg from "pg";
 import cors from "cors";
 
 const app = express();
 const port = 3000;
+env.config({ path: "../.env" });
 
 const db = new pg.Client({
-    user: "postgres",
-    host: "localhost",
-    database: "GT7Tracker",
-    password: "dbPractice",
-    port: 5432
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT
 });
 db.connect();
 
